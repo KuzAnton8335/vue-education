3a4ecc63c6c95d8430a7f0d0079c3bb7d3855b29
KuzAnton8335 <ant2783@mail.com>
2024-07-28 21:19:11 +0300

    first app-vue

---
+++ app.js
@@ -0,0 +1,31 @@
+// создание приложения при помощи Vue
+const App = {
+	// данные для работы с Vue {обьект}
+	data() {
+		return {
+			title: "Список заметок",
+			placeholderString: "Введите название заметки",
+			inputValue: '',
+			notes: ['Заметка 1']
+		}
+	},
+	//методы в Vue
+	methods: {
+		// вывод текстового поля input
+		inputChangeHandler(event) {
+			this.inputValue = event.target.value
+		},
+		// добавление заметки
+		addNewNote() {
+			this.notes.push(this.inputValue)
+			this.inputValue = ''
+		},
+		// удаление заметки
+		removeNote(idx) {
+			this.notes.splice(idx, 1)
+		}
+	}
+}
+// создаем переменную app и монтируем данные в html при помощи app
+const app = Vue.createApp(App).mount("#app");
+// ? остановился на времени 1:32:23
